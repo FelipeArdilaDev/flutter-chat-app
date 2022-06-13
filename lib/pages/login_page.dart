370690789639d88f623781a5d5c838dae91a8ead
felipe.ardila <felipe.ardila@valid.com>
2022-06-13 11:11:24 -0500

    Login y Registro terminado

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,79 @@
+import 'package:chat_app/widgets/boton_azul.dart';
+import 'package:chat_app/widgets/custom_input.dart';
+import 'package:chat_app/widgets/custom_labels.dart';
+import 'package:chat_app/widgets/custom_logo.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Logo(titulo: 'Messenger'),
+                  _Form(),
+                  const Labels(
+                    ruta: 'register',
+                    titulo: '¿No tienes cuenta?',
+                    subTitulo: 'Crea una ahora!',
+                  ),
+                  const Text(
+                    'Términos y condiciones de uso',
+                    style: TextStyle(fontWeight: FontWeight.w200),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.email_outlined,
+            placeholder: 'Email',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+            isPassword: false,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline_rounded,
+            placeholder: 'Contraseña',
+            keyboardType: TextInputType.visiblePassword,
+            textController: passCtrl,
+            isPassword: true,
+          ),
+          BotonAzul(
+            text: 'Ingresar',
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,88 @@
+import 'package:chat_app/widgets/boton_azul.dart';
+import 'package:chat_app/widgets/custom_input.dart';
+import 'package:chat_app/widgets/custom_labels.dart';
+import 'package:chat_app/widgets/custom_logo.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Logo(titulo: 'Registro'),
+                  _Form(),
+                  Labels(
+                    ruta: 'login',
+                    titulo: '¿Ya tienes una cuenta?',
+                    subTitulo: 'Ingresa ahora',
+                  ),
+                  const Text(
+                    'Términos y condiciones de uso',
+                    style: TextStyle(fontWeight: FontWeight.w200),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  final nameCtrl = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.perm_identity_outlined,
+            placeholder: 'Nombre',
+            keyboardType: TextInputType.text,
+            textController: nameCtrl,
+            isPassword: false,
+          ),
+          CustomInput(
+            icon: Icons.email_outlined,
+            placeholder: 'Email',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+            isPassword: false,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline_rounded,
+            placeholder: 'Contraseña',
+            keyboardType: TextInputType.visiblePassword,
+            textController: passCtrl,
+            isPassword: true,
+          ),
+          BotonAzul(
+            text: 'Ingresar',
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+              print(nameCtrl.text);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
